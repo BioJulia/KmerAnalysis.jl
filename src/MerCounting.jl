272b29a7112d9a9fa7adda3255e1033a8b07b89c
Ben J. Ward <benjward@protonmail.com>
2020-04-01 11:02:33 +0100

    Add single thread ram destroyer

--- src/MerCounting.jl
+++ src/MerCounting.jl
@@ -9,7 +9,10 @@ export
     freq,
     merge,
     collapse_into_counts,
-    collapse_into_counts!
+    collapse_into_counts!,
+    
+    # Single thread in RAM kmer counter.
+    count_mers_srd
 
 using
     BioSequences,
@@ -23,5 +26,6 @@ include("MerCount.jl")
 include("counter/MinimizerTable")
 include("MerCountHist.jl")
 include("IndexedCounts.jl")
+include("counters/srd.jl")
 
 end # module
